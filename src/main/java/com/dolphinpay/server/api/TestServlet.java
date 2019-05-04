c12181bd23b3f0615e41f1d1e45b1e1c07ec9d78
Vladislav Bogdan <vladislav.bogdan97@gmail.com>
2019-05-04 21:45:32 +0200

    Firebase configured

--- src/main/java/com/dolphinpay/server/api/TestServlet.java
+++ src/main/java/com/dolphinpay/server/api/TestServlet.java
@@ -5,24 +5,18 @@
  */
 package com.dolphinpay.server.api;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
+import com.dolphinpay.server.hibernate.HibernateUtils;
+import com.dolphinpay.server.hibernate.models.StandsOrdersState;
+import org.hibernate.Session;
 
-import java.io.*;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.util.List;
 
 public class TestServlet extends HttpServlet {
 
@@ -30,36 +24,47 @@ public class TestServlet extends HttpServlet {
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, URISyntaxException {
-        
+        try (PrintWriter out = response.getWriter()) {
+            out.println("aaaa");
+            try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+                out.println("ciaoooo");
+                List<StandsOrdersState> students = session.createQuery("from stands_orders_states", StandsOrdersState.class).list();
+ 
+                out.println(students.size());
+            } catch (Exception e) {
+                out.println("ERROR" + e.toString()); 
+            }
+        }
+
+        /*
         String token = "eXyUcS9WPVI:APA91bHORz_rH-T_DONu7EGbjwAS3mxombbrYotyWC4gmubxoEtBAOJWF8SaMg5TlOtQLrXyK9bGf2Fx3xU5ZszgLK5DVJLmJ0KymVoUKCSNLwqLFU7uVLqT20Y0ODzs0AFaNHWMvIcv";
-     
+
         try {
             FirebaseMessaging.getInstance().send(Message.builder().setToken(token).build());
         } catch (FirebaseMessagingException ex) {
             Logger.getLogger(TestServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
+        */
 
 
-        try (PrintWriter out = response.getWriter()) {
-            out.println(token);
-        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -74,10 +79,10 @@ public class TestServlet extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
