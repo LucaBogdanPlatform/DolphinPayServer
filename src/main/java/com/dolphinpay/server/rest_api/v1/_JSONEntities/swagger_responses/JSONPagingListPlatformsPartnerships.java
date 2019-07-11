477add4fce30ee7415b508be9b8a0d1dfe90dd2b
Vladislav Bogdan <vladislav.bogdan97@gmail.com>
2019-07-11 16:45:35 +0200

    Swagger 2 configured

---
+++ src/main/java/com/dolphinpay/server/rest_api/v1/_JSONEntities/swagger_responses/JSONPagingListPlatformsPartnerships.java
@@ -0,0 +1,7 @@
+package com.dolphinpay.server.rest_api.v1._JSONEntities.swagger_responses;
+
+import com.dolphinpay.server.rest_api.v1._JSONEntities.JSONPagingList;
+import com.dolphinpay.server.rest_api.v1.platforms_partenerships.PlatformPartnerships;
+
+public class JSONPagingListPlatformsPartnerships extends JSONPagingList<PlatformPartnerships> {
+}
--- src/main/java/com/dolphinpay/server/rest_api/v1/platforms_partenerships/PlatformPartnershipsAPI.java
+++ src/main/java/com/dolphinpay/server/rest_api/v1/platforms_partenerships/PlatformPartnershipsAPI.java
@@ -4,7 +4,11 @@ import com.dolphinpay.server.rest_api.v1.UtilsV1;
 import com.dolphinpay.server.rest_api.v1._JSONEntities.JSONAuthCredentials;
 import com.dolphinpay.server.rest_api.v1._JSONEntities.JSONLinks;
 import com.dolphinpay.server.rest_api.v1._JSONEntities.JSONPagingList;
+import com.dolphinpay.server.rest_api.v1._JSONEntities.swagger_responses.JSONPagingListPlatformsPartnerships;
 import com.dolphinpay.server.rest_api.v1.users.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,6 +37,17 @@ public class PlatformPartnershipsAPI {
     private PlatformPartnershipsService platformPartnershipsService;
 
     @GetMapping(UtilsV1.URLS.platformsPartnerships)
+    @ApiOperation(
+            value = "Get all available partnerships platforms",
+            response = JSONPagingListPlatformsPartnerships.class
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 400, message = "Invalid token or email or offset or count param"),
+                    @ApiResponse(code = 401, message = "Token elapsed time")
+            }
+    )
+
     public ResponseEntity getAllPaged(
             @RequestParam String token,
             @RequestParam String email,
